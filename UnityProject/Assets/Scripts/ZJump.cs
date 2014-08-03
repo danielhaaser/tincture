fdd2f628b4c3f6b22381fe952031f23a5c20f36c
djh- <thedapperdan@gmail.com>
2014-08-03 10:04:07 -0700

    Twitch effect on particles

--- UnityProject/Assets/Scripts/ZJump.cs
+++ UnityProject/Assets/Scripts/ZJump.cs
@@ -19,8 +19,15 @@ public class ZJump : MonoBehaviour {
 		float bassPower = audioAnalyzer.simpleBands [0];
 
 		Vector3 newPosition = new Vector3 (this.transform.localPosition.x, this.transform.localPosition.y, bassPower * 2.0f);
-
 		this.transform.localPosition = newPosition;
+
+		float squash = 1.0f;
+		if (bassPower > 0.3f)
+		{
+			squash =  1.0f - (bassPower -0.3f);
+		}
+		Vector3 newScale = new Vector3 (this.transform.localScale.x, squash, squash);
+		this.transform.localScale = newScale;
 
 	}
 }
