1290a3e1243146ea94c740bc77ebe3e2dda038ca
Zachary <zbarryte@mit.edu>
2014-08-03 04:38:22 -0700

    colors reduced to purples and teals

--- UnityProject/Assets/Prettiness/Scripts/ParticleFieldScript.cs
+++ UnityProject/Assets/Prettiness/Scripts/ParticleFieldScript.cs
@@ -8,7 +8,7 @@ public class ParticleFieldScript : MonoBehaviour {
 	public float mid;
 	public float treble;
 
-	private float progress;
+	private System.Random random = new System.Random();
 
 	public GameObject audioScriptGameObject;
 	private AudioAnalyzer audioAnalyzer;
@@ -25,12 +25,10 @@ public class ParticleFieldScript : MonoBehaviour {
 		mid = audioAnalyzer.simpleBands [1];
 		treble = audioAnalyzer.simpleBands [2];
 
-		progress += Time.deltaTime * 0.25f;
-
 		// COLOR
-		float r = 1.0f - Mathf.Sin(bass + mid * 2.0f + treble * 3.0f);
-		float g = 1.0f - Mathf.Sin(bass * 2.0f + mid * 2.0f + treble);
-		float b = 1.0f - Mathf.Sin(bass * 4.0f + mid + treble );
+		float r = 1.0f - Mathf.Sin(bass + treble * 2.0f);
+		float g = 1.0f - Mathf.Sin(mid + treble);
+		float b = 1.0f - Mathf.Sin(treble );
 
 		ParticleSystem.Particle[] particles = new ParticleSystem.Particle[particleSystem.particleCount];
 		particleSystem.GetParticles(particles);
@@ -40,7 +38,7 @@ public class ParticleFieldScript : MonoBehaviour {
 
 			// COLOR
 			particle.color = new Color32((byte)(r*255),(byte)(g*255),(byte)(b*255),255);
-			particle.size = treble * 50.0f;
+			particle.size = treble * 22.0f;
 
 			// write over the retreived particle
 			particles[i] = particle;
@@ -48,7 +46,11 @@ public class ParticleFieldScript : MonoBehaviour {
 		// write over the retreived particle array
 		particleSystem.SetParticles(particles,particleSystem.particleCount);
 
-		float localScale = 100.0f + 100.0f * Mathf.Cos (bass);
-		gameObject.transform.localScale = new Vector3(localScale,localScale,localScale);
+		// set a random velocity for the next particle
+		particleSystem.startSpeed = random.Next(0,22);
+
+		// scale to match the bass
+		float localScale = 222.0f + 222.0f * Mathf.Cos (bass + treble);
+		particleSystem.transform.localScale = new Vector3(localScale,localScale,localScale);
 	}
 }
